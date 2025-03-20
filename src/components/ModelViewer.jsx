5c17c47d52f13b0514c06eafd97236ca7f925787
Esteban Torres <rockiesteban@gmail.com>
2025-03-19 21:19:00 -0500

    Ajustes modelo 3D

--- src/components/ModelViewer.jsx
+++ src/components/ModelViewer.jsx
@@ -1,28 +1,30 @@
 'use client';
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, useGLTF, Environment } from '@react-three/drei';
+import { OrbitControls, useGLTF, Environment, Bounds } from '@react-three/drei';
 
 const Model = () => {
-  // Ruta relativa al modelo en la carpeta /public
   const gltf = useGLTF('/proyectos/mueble_closet_melamina.glb');
-  
-  return <primitive object={gltf.scene} scale={1} />;
+
+  return <primitive object={gltf.scene} />;
 };
 
 const ModelViewer = () => {
   return (
     <div style={{ width: '80%', height: '700px' }}>
-      <Canvas camera={{ position: [2, 2, 2], fov: 50 }}>
+      <Canvas>
         <ambientLight intensity={0.5} />
         <directionalLight position={[2, 2, 2]} intensity={1} />
-        
+
         <Suspense fallback={null}>
           <Environment preset="city" />
-          <Model />
+
+          {/* Bounds ajusta el encuadre automático */}
+          <Bounds fit clip observe margin={1.2}>
+            <Model />
+            <OrbitControls makeDefault />
+          </Bounds>
         </Suspense>
-        
-        <OrbitControls enableZoom={true} />
       </Canvas>
     </div>
   );
